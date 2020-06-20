212d8d8eeacc43145bff5cee6ea6f3ed0b0b6c2a
Socha <citrineplus@gmail.com>
2020-06-19 18:07:58 -0700

    added trash, holding tables, player obj player knows what they're holding

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 //main file for actual game
 
+// global player Object
+var user =
+{
+  xcoord : 0,
+  ycoord : 0,
+  holding : 0,
+  dishArr : []
+}
 // player properties
 let player = document.querySelector('.player');
-let moveBy = 10;
+let moveBy = 30;
 
 // setting initial position
 window.addEventListener('load', () => {
@@ -82,17 +90,69 @@ function getMidpoint(element){
     return coords;
 }
 
+function isClose(element1, element2) {
+  if (Math.abs(getMidpoint(element1).x - getMidpoint(element2).x) < 50 && Math.abs(getMidpoint(element1).y - getMidpoint(element2).y) < 50) {
+    return true;
+  }
+  return false;
+}
+
 function preformAction(elementName){
     // check if player and button pressed are close to each other
     var player;
     var element;
 
+    var tempelement;
+    var temp2;
+    // put this in another function
     player = document.getElementById('player');
     element = document.getElementById(elementName);
 
-    // check that the player is close enough to the element
-    if (Math.abs(getMidpoint(element).x - getMidpoint(player).x) < 10 && Math.abs(getMidpoint(element).y - getMidpoint(player).y) < 10) {
-      console.log("good");
-      element.style.backgroundColor = 'gray';
+    // exit function is the player is not close enough to preform the action
+    if (!(isClose(player, element))) {
+        return false;
+    }
+
+    // switch depending on element name
+    if(elementName == "bread" || elementName == "espresso" || elementName == "cookies") {
+      tempelement = document.getElementById('hidden' + elementName);
+      tempelement.style.visibility = 'visible';
+    }
+
+    else if(elementName == "hiddenbread" || elementName == "hiddenespresso" || elementName == "hiddencookies") {
+      tempelement = document.getElementById(elementName);
+      tempelement.style.visibility = 'hidden';
+
+      // display purposes
+      player.innerHTML = elementName[6] + '0';
+      user.holding = elementName[6] + '0'
+    }
+
+    else if (elementName == "slot1" || elementName == "slot2" || elementName == "slot3") {
+      tempelement = document.getElementById(elementName);
+      if (user.holding != 0) {
+
+        if (tempelement.innerHTML != elementName) {
+          player.innerHTML = tempelement.innerHTML;
+          temp2 = tempelement.innerHTML;
+          tempelement.innerHTML = user.holding;
+          user.holding = temp2;
+
+        }
+
+        else {
+          player.innerHTML = 'me';
+          tempelement.innerHTML = user.holding;
+          user.holding = 0;
+        }
+
+
+      }
+      else if((user.holding == 0) && tempelement.innerHTML != elementName) {
+        player.innerHTML = tempelement.innerHTML;
+        user.holding = tempelement.innerHTML;
+        tempelement.innerHTML = elementName;
+      }
+
     }
 }
